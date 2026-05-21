bf21cdeb071c69946d85698ac4c50a1288c5b7f6
keerthan <poojarykeerthan571@gmail.com>
2026-05-21 19:32:29 +0530

    added css for checkoutPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
+import { CheckoutPage } from './pages/CheckoutPage'
 function App(){
   return (
-    <HomePage />
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />}></Route>
+    </Routes>
+   
   )
 }
 export default App
